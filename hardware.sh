c8e59ca54a9128d6fc22969dc9b2a9c2fe89be99
chaslinux <chaslinux@gmail.com>
2023-02-24 23:16:50 -0500

    To be filled by O.E.M. in serial field should now result in the system's MAC address showing as the barcode

--- hardware.sh
+++ hardware.sh
@@ -48,7 +48,7 @@ printf '\\maketitle\n' >> /home/"$USER"/Desktop/specs.tex
 # if no OEM barcode, use mac address: cat /sys/class/net/*/address | head -n 1 >> /home/"$USER"/Desktop/barcode.txt
 # Wrap bottom statement in an IF statement or maybe set this as a varable before
 # 02/14/2023 - Happy Valentines Day - if SLEN is less than 4 characters it's not a proper serial number, use mac address
-if [[ $SLEN -lt 4 || $SERIALNO == "System Serial Number" ]]
+if [[ $SLEN -lt 4 || $SERIALNO == "System Serial Number" || $SERIANO == "To be filled by O.E.M." ]]
 	then
 		echo "$FAMILY"
 		cat /sys/class/net/*/address | head -n 1 | sed 's/://g' >> /home/"$USER"/Desktop/barcode.txt
