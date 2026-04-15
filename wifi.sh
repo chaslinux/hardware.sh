8b86afede051b72e7ed8af071876acb611963936
Nosey Nick <NoseyNick@users.noreply.github.com>
2026-04-15 13:04:03 -0400

    Nick adding UNTESTED wifi 1/2/3 support (#4)
    * ATTEMPT to support WiFi 1/2/3 detection
    * add myself to comments, as requested

--- wifi.sh
+++ wifi.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -efu
 ######################################################################
-# What WiFi technology(ies) does this Linux machine have?
-# Written by "Nosey" Nick for / (C)opyright 2026 CR @ TWC
+# What WiFi technology/generation does this Linux machine support?
+# Written by NoseyNick, wifi-sh@noseynick.net
+# for / (C)opyright 2026 CR @ TWC
 # All wrong righted. All rights reserved.
+# Licensed under GPLv3, the General Public License v3.0
 ######################################################################
 # ++++++++++ TODO: Support for MULTIPLE cards by breaking on "Wiphy phy2" lines?
 
@@ -26,18 +28,17 @@ echo Scanning for WiFi capabilities... >&2
 # Table of different WiFi types, and how to recognise them by using
 # the above iwgrep. Ref https://en.wikipedia.org/wiki/IEEE_802.11
 
-###    TECH GEN 802.xx # notes
-# ++++++++++ Test / add 8 / 9 here?
-iwgrep  UHR  8 "11bn, or greater" # UNTESTED 
-iwgrep  EHT  7  11be   # UNTESTED - "Extremely High Throughput" - WiFi 7
-#            6e 11ax   ? 6E adds 6GHz channels?
-iwgrep  HE   6  11ax   # UNTESTED "High Efficiency" - WiFi 6
-iwgrep  VHT  5  11ac   #   TESTED "Very High Throughput" implies WiFi 5
-iwgrep  HT   4  11n    #   TESTED "High Throughput" indicating WiFi 4
-#            3  11g    # grep for what?
-#            2g 11a    # grep for what?
-#            2  11b    # grep for what?
-#            1  11     # grep for what?
+###     TECH      GEN 802.xx # notes # ++++++++++ Add + test 9/10/11 here?
+iwgrep  UHR        8 "11bn, or greater" # UNTESTED
+iwgrep  EHT        7  11be   # UNTESTED - "Extremely High Throughput" - WiFi 7
+iwgrep  "6.* MHz"  6e 11ax   # UNTESTED - anything better to grep than freqs?
+iwgrep  HE         6  11ax   # UNTESTED "High Efficiency" - WiFi 6
+iwgrep  VHT        5  11ac   #   TESTED "Very High Throughput" implies WiFi 5
+iwgrep  HT         4  11n    #   TESTED "High Throughput" indicating WiFi 4
+iwgrep "54.0 Mbps" 3  11g    # UNTESTED - anything better to grep than speed?
+iwgrep  "5.* MHz"  2g 11a    # UNTESTED - anything better to grep than freqs?
+iwgrep "11.0 Mbps" 2  11b    # UNTESTED - anything better to grep than speed?
+iwgrep  "1.0 Mbps" 1  11     # UNTESTED - anything better to grep than speed?
 
 echo "ERR: Does this even have a WiFi card?!? Check: iw phy" >&2
 exit 1
